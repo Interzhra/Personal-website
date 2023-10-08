a173b96576df29dded9faad84bae1f6befddfa36
Interzhra <147293356+Interzhra@users.noreply.github.com>
2023-10-08 10:53:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("youtube-button").addEventListener("click", function() {
+    // Ganti URL di bawah ini dengan URL YouTube yang diinginkan
+    var youtubeURL = "https://youtube.com/@osisskansa73?si=--1WgDzIA9rD_CvX"; 
+    window.open(youtubeURL, "_blank");
+});
+
+document.getElementById("web-button").addEventListener("click", function() {
+    // Ganti URL di bawah ini dengan URL website yang diinginkan
+    var websiteURL = "https://smkn1pengasih.sch.id/"; 
+    window.open(websiteURL, "_blank");
+});
